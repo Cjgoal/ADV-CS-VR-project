3d96ee9430d40a64610c4e90c98a72b4d6c045fd
Cjgoal <Colinjenn123@gmail.com>
2025-03-06 20:43:11 -0800

    adasda

--- game/objects/storage/storage_box.gd
+++ game/objects/storage/storage_box.gd
@@ -3,9 +3,12 @@ extends StaticBody3D
 # List of objects and their values
 var object_values = {
 	"Wine": 50,
-	"pool_table": 100,
-	"RubixCube": 75,
-	"guitar": 150
+	"Pool Table": 100,
+	"Rubix Cube": 75,
+	"Rubix Cube2": 50,
+	"Guitair": 150,
+	"Coffee maker":75
+	
 }
 
 # Set to track detected objects (to avoid detecting each item more than once)
@@ -16,14 +19,7 @@ func _ready():
 	$DropZone.body_entered.connect(_on_item_entered)
 
 func _on_item_entered(body):
-		if body.name not in detected_items:
-			detected_items[body.name] = true  # Mark this object as detected
-
-			# Determine the object's value based on its name
-			var object_value = object_values.get(body.name, 0)  # Default value 0 if not in list
-
-			# Update the score
-			Globals.Score += object_value
-			print("Detected:", body.name, "Value:", object_value)
-		else:
-			print("Item already detected:", body.name)
+	print(body.name)
+	var object_value = object_values.get(body.name, 0)  # Default value 0 if not in list
+	print(object_value)
+	Globals.Game_Score += object_value
