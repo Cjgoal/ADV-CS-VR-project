e095ae457640f196a4fc281db36c27e152ac227f
Cjgoal <Colinjenn123@gmail.com>
2025-03-06 20:26:00 -0800

    asdas

--- game/objects/storage/storage_box.gd
+++ game/objects/storage/storage_box.gd
@@ -1,11 +1,29 @@
 extends StaticBody3D
 
+# List of objects and their values
+var object_values = {
+	"Wine": 50,
+	"pool_table": 100,
+	"RubixCube": 75,
+	"guitar": 150
+}
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# Set to track detected objects (to avoid detecting each item more than once)
+var detected_items = {}
 
+func _ready():
+	# Connect the area detection signals
+	$DropZone.body_entered.connect(_on_item_entered)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_item_entered(body):
+		if body.name not in detected_items:
+			detected_items[body.name] = true  # Mark this object as detected
+
+			# Determine the object's value based on its name
+			var object_value = object_values.get(body.name, 0)  # Default value 0 if not in list
+
+			# Update the score
+			Globals.Score += object_value
+			print("Detected:", body.name, "Value:", object_value)
+		else:
+			print("Item already detected:", body.name)
